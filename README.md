9c1e48f25d40c6a2662ff3881749968290ac554c
laurapease <laurapeasetm@gmail.com>
2020-09-25 12:34:59 -0500

    Added animation to progress meters to increase over time.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Project-0
 First project for SEI = Tamagotchi Virtual Pet
+Game Logic:
+
+- create 3 ages for pet = Baby, Adult, Elder
+- prompt: What is your name? Welcome player to game
+- Input box for pet's name. Update the page with pet's name once button is clicked. 
+- Buttons for feed, sleep, play - connect these to hunger, sleepiness, boredom levels
+- Connect feed, sleep, play buttons to hunger Level, Tiredness Level and Boredom Level.
+- Update age as the game progresses and pet evolves
+- game over if hunger, boredom, sleepiness hit 0
+
+Timer:
+Start at 3000 seconds. When it hits 2000, evolve. When it hits 1000, evolve
+- Game also over when timer reaches zero
+
+Wellness Meters:
+All start at 0.
+Increase by 1 every 100s
+
+Sleep button: 
+Changes background to dark background when clicked.
+Decreases 'sleep meter' by 1 when clicked
--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,14 @@
+// Baby cat url: https://imgur.com/zU2FrKo
+// adult cat url: https://imgur.com/roGBcpz
+// elder cat url: https://imgur.com/wLbZHfr
+
+// baby dog url: https://imgur.com/Qqv15yM
+// adult dog url: https://imgur.com/79uPelw
+// elder dog url: https://imgur.com/ZHCoxvs
+
+// background image if i toggle to a diffent img for sleep: img src="https://images-na.ssl-images-amazon.com/images/I/71ut-GH-boL._AC_SX522_.jpg"'
+//   );
+
 // Specifications
 // Create a repo for your tomagotchi pet
 // make a commit after you finish each one of the following
@@ -14,7 +25,7 @@
 // Style the page.
 // Increase your pet's age every x minutes
 // Increase your pet's Hunger, Sleepiness, and Bored metrics on an interval of your choosing.
-// You pet should die if Hunger, Boredom, or Sleepiness hits 10.
+// You pet should die if Hunger, Boredom, or Sleepiness hits 0.
 // Morph your pet at certain ages.
 // Animate your pet across the screen while it's alive.
 
@@ -26,28 +37,6 @@ $("#start").on("click", function () {
     `Hi ${name}! Name your pet in the input below, then it's time to go meet your new family member!!!`
   );
 });
-
-class Pet {
-  constructor(name, age) {
-    this.hunger = 0;
-    this.sleepiness = 0;
-    this.boredom = 0;
-    this.age = age;
-    this.name = name;
-  }
-}
-// figure out how to name pet what the user enters into prompt//
-
-const Baby = new Pet("Tank", 5);
-const Adult = new Pet("Boots", 10);
-const Elder = new Pet("Big Bird", 15);
-
-console.log(Baby, Adult, Elder);
-
-// end of game ?
-// if (this.hunger >= 10 || this.sleepiness >= 10 || this.boredom >= 10) {
-//   console.log("Game over!");
-//}
 
 //https://stackoverflow.com/questions/17433557/how-to-save-user-input-into-a-variable-in-html-and-js
 
@@ -63,16 +52,41 @@ function getUserName() {
     //alert(nameField);
   }
 }
+
+class Pet {
+  constructor(name, age) {
+    this.hunger = 0;
+    this.sleepiness = 0;
+    this.boredom = 0;
+    this.age = age;
+    this.name = name;
+  }
+}
+// figure out how to name pet what the user enters into prompt//
+
+const Baby = new Pet(name, 5);
+const Adult = new Pet(name, 10);
+const Elder = new Pet(name, 15);
+
+console.log(Baby, Adult, Elder);
+
+// end of game ?
+// if (this.hunger >= 10 || this.sleepiness >= 10 || this.boredom >= 10) {
+//   console.log("Game over!");
+//}
+
 var subButton = document.getElementById("subButton");
 subButton.addEventListener("click", getUserName, false);
 
-function hide() {
-  let div = document.getElementyById("subbutton");
-  div.style.display = "none";
-}
+//Submit name buton dissappears when clicked - IT WORKS!
+
+$("#subButton").on("click", (event) => {
+  // $(event.currentTarget) refers to the '#menu' element that was clicked.
+  $(event.currentTarget).hide();
+});
 
 function timer() {
-  var sec = 300;
+  var sec = 100;
   var timer = setInterval(function () {
     document.getElementById("countdown").innerHTML = "00:" + sec;
     sec--;
@@ -114,19 +128,148 @@ timer();
 // countdownTimer();
 // setInterval(countdownTimer, 1000);
 
-//Button Functions  trying to
+//Button Functions
+
+//use jquery .fadeToggle to make food fade in and out when clicked
+// Example:
+//$('#menu-button').on('click', () => {
+// ( equivalent to )
+//$('#menu').fadeToggle(400, 'swing')
+// $('#menu').fadeToggle();
+// });
 
 $("#feed").click(function () {
   Pet.food += 1;
 });
 
+//use jquery .fadeToggle to fade in and out a toy when button is c;icked
+
 $("#play").click(function () {
   Pet.activity += 1;
 });
 
-$("#sleep").click(function () {
-  Pet.sleep += 1;
-});
+// Potentially use sligeToggle to make the moon rise and fall when sleep is clicked. Also, change opacity so the screen darkens when sleep is clicked. Do the opposite for wake up (or take wake up out if I can set a timer of sorts on this function.
+// $('#sleep').on('click', () => {
+//     $('#sleep').slideToggle();
+// // });
+
+//TRYING TO CHANGE BACKGROUND WHEN SLEEP BUTON IS CLICKED...WHERE CAN I STORE THE BACKGROUNG IMG AS AN ELEMENT, CLASS OR ID??
+
+// EXAMPLE 2:
+// $(document).ready(function () {
+//   // Set background image of a div on click of the button
+//   $("#sleep").click(function () {
+//     var imageUrl =
+//       "https://images-na.ssl-images-amazon.com/images/I/71ut-GH-boL._AC_SX522_.jpg";
+//     $("body").css("background-image", "url(" + "https:/images-na.ssl-images-amazon.com/images/I/71ut-GH-boL._AC_SX522_.jpg" + ")");
+//   );
+// });
+
+// $("#sleep").click(function () {
+//   $("backgounde").toggle("slow", 0.5, function () {});
+// });
+
+//????????
+// $("#sleep").on("click", function () {
+//   //   Pet.sleep += 1;
+//   $("#sleep").css(
+//     "background-image",
+//     'img src="https://images-na.ssl-images-amazon.com/images/I/71ut-GH-boL._AC_SX522_.jpg"'
+//   );
+// });
+
+///progress meters - this works and the bar slowly gets bigger - need to figure out how to make smaller when buttons are clicked
+//https://www.w3schools.com/howto/howto_js_progressbar.asp
+
+// HUNGER METER
+
+var i = 0;
+function moveHunger() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("hunger");
+    var width = 1;
+    var id = setInterval(frame, 1000);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
+
+// moveHunger();
+
+//SLEEPINESS METER
+
+var i = 0;
+function moveSleepiness() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("sleepiness");
+    var width = 1;
+    var id = setInterval(frame, 1000);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
+
+// moveSleepiness();
+
+var i = 0;
+function moveBoredom() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("boredom");
+    var width = 1;
+    var id = setInterval(frame, 1000);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
+
+// moveBoredom();
+
+// AGE LEVEL
+
+var i = 0;
+function moveAge() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("age");
+    var width = 1;
+    var id = setInterval(frame, 3000);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
+
+// moveAge();
 
 class Person {
   constructor(name, age, eyes, hair) {
