9a3813e203cc8b0a3847636214baea243e5f024a
laurapease <laurapeasetm@gmail.com>
2020-09-27 18:23:21 -0500

    Buttons linked for feed, sleep, eat

--- scripts/app.js
+++ scripts/app.js
@@ -31,6 +31,91 @@
 
 ///Start game prompt when click Adopt Pet Button
 
+$("#start").on("click", function () {
+  let name = prompt("What is your name?");
+  alert(
+    `Hi ${name}! Meet your new pet! We call them Charmander, but you can name them whatever you'd like. First, click on 'Adopt Charmander.' Then name your pet. The timer will start once you've added your pet's name!!`
+  );
+});
+
+// FEED BUTTON
+
+//SOURCE: https://stackoverflow.com/questions/17433557/how-to-save-user-input-into-a-variable-in-html-and-js
+
+function getUserName() {
+  let nameField = document.getElementById("nameField").value;
+  let result = document.getElementById("result");
+
+  if (nameField.length < 3) {
+    result.textContent = "Username must contain at least 3 characters";
+    //alert('Username must contain at least 3 characters');
+  } else {
+    result.textContent = "Your pet's name is: " + nameField;
+    //alert(nameField);
+  }
+}
+
+let hunger = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+let sleepiness = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+let boredom = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+let name;
+
+let sleep = function () {
+  Charmander.sleepiness -= 1;
+  console.log("Nap time");
+};
+// wake() {
+//   console.log("Wakey wakey!");
+// }
+
+let play = function () {
+  Charmander.boredom -= 1;
+  console.log("What fun!");
+};
+
+class Pet {
+  constructor(name, hunger, sleepiness, boredom, age) {
+    this.name = name;
+    this.hunger = hunger;
+    this.sleepiness = sleepiness;
+    this.boredom = boredom;
+    this.age = age;
+  }
+}
+
+let Charmander = new Pet((name = "Charmander"), hunger, sleepiness, boredom, 0);
+// let Abra = new Pet(name, hunger, sleepiness, boredom, 0);
+// let Charmander = new Pet(name, hunger, sleepiness, boredom, 0);
+
+console.log(Charmander);
+
+const chooseName = document.getElementById("subButton");
+
+chooseName.addEventListener("click", petAge);
+let ageLevel = document.getElementById("age-level");
+function petAge() {
+  ageLevel.textContent = Charmander.age;
+}
+chooseName.addEventListener("click", petHunger);
+let hungerLevel = document.getElementById("hunger-level");
+function petHunger() {
+  hungerLevel.textContent = Charmander.hunger;
+}
+
+chooseName.addEventListener("click", petBoredom);
+let boredomLevel = document.getElementById("boredom-level");
+function petBoredom() {
+  boredomLevel.textContent = Charmander.boredom;
+}
+
+chooseName.addEventListener("click", petSleepiness);
+let sleepinessLevel = document.getElementById("sleepiness-level");
+function petSleepiness() {
+  sleepinessLevel.textContent = Charmander.sleepiness;
+}
+
+//TIMER
+
 function timer() {
   var sec = 200;
   var timer = setInterval(function () {
@@ -67,80 +152,31 @@ function timer() {
   }, 1000);
 }
 
-$("#start").on("click", function () {
-  let name = prompt("What is your name?");
-  alert(
-    `Hi ${name}! Meet your new pet! We call them Charmander, but you can name them whatever you'd like. First, click on 'Adopt Charmander.' Then name your pet. The timer will start once you've added your pet's name!!`
-  );
-});
-// FEED BUTTON
-
-//SOURCE: https://stackoverflow.com/questions/17433557/how-to-save-user-input-into-a-variable-in-html-and-js
-
-function getUserName() {
-  let nameField = document.getElementById("nameField").value;
-  let result = document.getElementById("result");
-
-  if (nameField.length < 3) {
-    result.textContent = "Username must contain at least 3 characters";
-    //alert('Username must contain at least 3 characters');
-  } else {
-    result.textContent = "Your pet's name is: " + nameField;
-    //alert(nameField);
-  }
-}
-
-let hunger = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
-let sleepiness = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
-let boredom = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
-let name;
-
-let eat = function () {
-  this.hunger -= 1;
-  console.log("Yum!");
-};
-let sleep = function () {
-  this.sleepiness -= 1;
-  console.log("Nap time");
-};
-// wake() {
-//   console.log("Wakey wakey!");
-// }
-
-let play = function () {
-  this.boredom -= 1;
-  console.log("What fun!");
-};
-
-class Pet {
-  constructor(name, hunger, sleepiness, boredom, age) {
-    this.name = name;
-    this.hunger = hunger;
-    this.sleepiness = sleepiness;
-    this.boredom = boredom;
-    this.age = age;
-  }
-}
-
-let Charmander = new Pet((name = "Charmander"), hunger, sleepiness, boredom, 0);
-// let Abra = new Pet(name, hunger, sleepiness, boredom, 0);
-// let Charmander = new Pet(name, hunger, sleepiness, boredom, 0);
-
-console.log(Charmander);
-
-let endOfGame = function () {
-  if (
-    Charmander.hunger >= 10 ||
-    Charmander.sleepiness >= 10 ||
-    Charmander.boredom >= 10 ||
-    Charmander.age >= 10
-  ) {
-    alert("Game over!");
-  }
-};
-endOfGame();
-
 //BUTTONS
+
+// FEED BUTTON DECREASES HUNGER BY 1 WHEN CLICKED
+
+let feedButton = document.getElementById("feed");
+function feedPet() {
+  Charmander.hunger = Charmander.hunger - 1;
+  console.log("Yum!");
+  hungerLevel.textContent = Charmander.hunger;
+}
+feedButton.addEventListener("click", feedPet);
+
+// SLEEP BUTTON DECREASES TIREDNESS BY 1 WHEN CLICKED
+
+let sleepButton = document.getElementById("sleep");
+function sleepPet() {
+  Charmander.tiredness = Charmander.tiredness - 1;
+  console.log("Bedtime!");
+  sleepinessLevel.textContent = Charmander.tiredness;
+}
+sleepButton.addEventListener("click", sleepPet);
+
+// PLAY BUTTON DECREASES BOREDNESS BY 1 WHEN CLICKED
+
+//EXERCSE BUTTON DECREASES BOREDNESS BY 1 AND NCREASES TIREDNESS BY 1 WHEN CLICKED
 
 // RESET GAME BUTTON
 
@@ -203,42 +239,11 @@ $("#charlizard").on("click", () => {
 //     <div class="meters" id="boredom">Boredom Level: <span id="boredom-level"></span></div>
 //     <div class="meters" id="age">Age: <span id="age-level"></span></div>
 
-const chooseName = document.getElementById("subButton");
-
-chooseName.addEventListener("click", petAge);
-let ageLevel = document.getElementById("age-level");
-function petAge() {
-  ageLevel.textContent = Charmander.age;
-}
-chooseName.addEventListener("click", petHunger);
-let hungerLevel = document.getElementById("hunger-level");
-function petHunger() {
-  hungerLevel.textContent = Charmander.hunger;
-}
-
-chooseName.addEventListener("click", petBoredom);
-let boredomLevel = document.getElementById("boredom-level");
-function petBoredom() {
-  boredomLevel.textContent = Charmander.boredom;
-}
-
-chooseName.addEventListener("click", petSleepiness);
-let sleepinessLevel = document.getElementById("sleepiness-level");
-function petSleepiness() {
-  sleepinessLevel.textContent = Charmander.sleepiness;
-}
-
 //use jquery .fadeToggle to fade in and out a toy when button is clicked
 
 $("#play").click(function () {
   Charmander.boredom = Charmander.boredom - 1;
 });
-
-let feed = document.getElementById("feed");
-function feedPet() {
-  Charmander.hunger = Charmander.hunger - 1;
-  alert("you fed me!");
-}
 
 // Potentially use sligeToggle to make the moon rise and fall when sleep is clicked. Also, change opacity so the screen darkens when sleep is clicked. Do the opposite for wake up (or take wake up out if I can set a timer of sorts on this function.
 // $('#sleep').on('click', () => {
@@ -270,3 +275,16 @@ function feedPet() {
 //https://www.w3schools.com/howto/howto_js_progressbar.asp
 
 // END OF GAME
+const endOfGame = function () {
+  if (
+    Charmander.hunger >= 10 ||
+    Charmander.sleepiness >= 10 ||
+    Charmander.boredom >= 10 ||
+    Charmander.age >= 10 ||
+    timer.val === 0
+  ) {
+    alert("Game over!");
+  }
+};
+
+endOfGame();
