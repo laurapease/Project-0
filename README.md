18d22c420f0412c7d4cb8dd1872648e771a6bbf6
laurapease <laurapeasetm@gmail.com>
2020-09-28 10:34:07 -0500

    A few final touches

--- README.md
+++ README.md
@@ -1,23 +1,38 @@
 # Project-0
-First project for SEI = Tamagotchi Virtual Pet
+
+\*\* First project for SEI = Tamagotchi Virtual Pet
+
 Game Logic:
 
-- create 3 ages for pet = Baby, Adult, Elder
-- prompt: What is your name? Welcome player to game
-- Input box for pet's name. Update the page with pet's name once button is clicked. 
-- Buttons for feed, sleep, play - connect these to hunger, sleepiness, boredom levels
-- Connect feed, sleep, play buttons to hunger Level, Tiredness Level and Boredom Level.
-- Update age as the game progresses and pet evolves
-- game over if hunger, boredom, sleepiness hit 0
+1. The featured pet is a 3 tier evolution of Charmander, a Pokeman character. Charmander will evolve when the player is prompted at at ages 3 and 6.
+2. To begin the game, the player clicks 'Start Game.' A prompt appears asking for the player name. Once entered, the player is welcomed to start the game.
+3. The player is instructed to click 'Adopt Charmander.' When clicked, the egg hatches to Charmander.
+4. Next, the player will input the name of their pet. The page will update with the pet's name once button is clicked, and the timer will start.
+5. Hunger, Sleepiness, and Boredom values are determined at random once the Name Pet button is clicked.
+6. The game lasts 200 seconds.
+7. Hunger, sleepiness and boredom all increase by 1 every 10 seconds.
+8. Age increases by 1 every 20 seconds.
+9. Buttons for feed, sleep, exercise and play are connected those of hunger, sleepiness, boredom levels
+10. Feed lowers Hunger Level by 1
+11. Sleep/Wake lowers Sleepiness Level by 1. The background changes to a nighttime scene when clicked, then back to saytime when clicked again.
+12. Let's Play lowers Boredom Level by 1
+13. Exercise lowers Boredom Level by 1 and raises Hunger Level by 1
+14. Connect feed, sleep, play buttons to hunger Level, Tiredness Level and Boredom Level.
+15. The player is prompted to click the Charmeleon button to evolve at 140 seconds (at age 3).
+16. The player is prompted to click the Charlizrd button to evolve at 80 seconds (at age 6).
+17. Game is over when timer hits 0.
+18. The 'Reset Game' button resets the browser to the original loaded page when clicked.
 
-Timer:
-Start at 3000 seconds. When it hits 2000, evolve. When it hits 1000, evolve
-- Game also over when timer reaches zero
+Animations:
 
-Wellness Meters:
-All start at 0.
-Increase by 1 every 100s
+1. Buttons have animations attached when the mouse hovers over them.
+2. The egg and pet evolutions are gifs found online in pixel art.
+3. The egg/pets wobble when the mouse hovers over them.
 
-Sleep button: 
-Changes background to dark background when clicked.
-Decreases 'sleep meter' by 1 when clicked
+Difficulties:
+
+1. I was not able to connect the end of game with any factors others than the timer reaching 0. At one point, I had it working where the player would get an alert of 'Game over' if any of the wellness meters reached 10, but somehow it stopped working.
+2. I toggled the class of the background when the Sleep button was pressed. I could change the html inside of the button to be 'Wake me' when it toggled to sleep, but it would not toggle back to 'sleep.' I chose to put both 'Sleep' and 'Wake' on the button at the same time to avoid confusion.
+3. I was not able to make sre the wellness meters didn't go below zero.
+4. I would have liked to have a fade in/out effect for evoltions, but I think I set up the image wrong from the start and couldn't make it work.
+5. I found animations difficult and I will continue to work on how to animate using @keyframes in order to make this work.
--- scripts/app.js
+++ scripts/app.js
@@ -1,44 +1,9 @@
-// background image if i toggle to a diffent img for sleep: img src="https://images-na.ssl-images-amazon.com/images/I/71ut-GH-boL._AC_SX522_.jpg"'
-//   );
-
-//charmander: img src "https://thumbs.gfycat.com/ShyImperturbableAlpaca-max-1mb.gif"
-
-// charmeleon img src "https://66.media.tumblr.com/tumblr_ma4fpfD6Tu1rfjowdo1_500.gif"
-
-//charlizard img src "https://66.media.tumblr.com/tumblr_ma4fsg8aDZ1rfjowdo1_500.gif"
-
-// ball art img when 'PLAY' is clicked for animation: https://www.pinclipart.com/picdir/middle/420-4200946_rainbow-beach-ball-pixel-art-sphere-clipart.png
-
-// Specifications
-// Create a repo for your tomagotchi pet
-// make a commit after you finish each one of the following
-// Create a Class(JS Class, look at your notes if your forget) for your tomagotchi
-// Instatiate your Tomagotchi
-// Display a character of your choice on the screen to represent your pet
-// Display the following metrics for your pet:
-// Hunger(1 - 10 scale)
-// Sleepiness(1 - 10 scale)
-// Boredom(1 - 10 scale)
-// Age
-// Add buttons to the screen to feed your pet, turn off the lights, and play with your pet.
-// Add the ability to name your pet.
-// Style the page.
-// Increase your pet's age every x minutes
-// Increase your pet's Hunger, Sleepiness, and Bored metrics on an interval of your choosing.
-// You pet should die if Hunger, Boredom, or Sleepiness hits 10.
-// Morph your pet at certain ages.
-// Animate your pet across the screen while it's alive.
-
-///Start game prompt when click Adopt Pet Button
-
 $("#start").on("click", function () {
   let name = prompt("What is your name?");
   alert(
     `Hi ${name}! Meet your new pet! We call them Charmander, but you can name them whatever you'd like. First, click on 'Adopt Charmander.' Then name your pet. The timer will start once you've added your pet's name!!`
   );
 });
-
-// FEED BUTTON
 
 //SOURCE: https://stackoverflow.com/questions/17433557/how-to-save-user-input-into-a-variable-in-html-and-js
 
@@ -59,19 +24,6 @@ let hunger = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
 let sleepiness = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
 let boredom = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
 let name;
-
-let sleep = function () {
-  Charmander.sleepiness -= 1;
-  console.log("Nap time");
-};
-// wake() {
-//   console.log("Wakey wakey!");
-// }
-
-let play = function () {
-  Charmander.boredom -= 1;
-  console.log("What fun!");
-};
 
 class Pet {
   constructor(name, hunger, sleepiness, boredom, age) {
@@ -124,7 +76,6 @@ function timer() {
     if (sec < 0) {
       clearInterval(timer);
     }
-
     if (sec === 140) {
       alert("Time to evolve to Charmeleon! Click the Charmeleon button!");
       console.log("Time to evolve to Charmeleon! Click the Charmeleon button!");
@@ -149,6 +100,9 @@ function timer() {
       Charmander.boredom += 1;
       petBoredom();
     }
+    if (sec === 0) {
+      alert("Time is up! End of game!");
+    }
   }, 1000);
 }
 
@@ -158,9 +112,12 @@ function timer() {
 
 let feedButton = document.getElementById("feed");
 function feedPet() {
-  Charmander.hunger = Charmander.hunger - 1;
-  console.log("Yum!");
-  hungerLevel.textContent = Charmander.hunger;
+  if (Charmander.hunger >= 0) {
+    Charmander.hunger = Charmander.hunger - 1;
+    alert("Yum!");
+    console.log("Yum!");
+    hungerLevel.textContent = Charmander.hunger;
+  }
 }
 feedButton.addEventListener("click", feedPet);
 
@@ -168,22 +125,51 @@ feedButton.addEventListener("click", feedPet);
 
 let sleepButton = document.getElementById("sleep");
 function sleepPet() {
-  Charmander.tiredness = Charmander.tiredness - 1;
-  console.log("Bedtime!");
-  sleepinessLevel.textContent = Charmander.tiredness;
+  if (Charmander.sleepiness >= 0) {
+    Charmander.sleepiness = Charmander.sleepiness - 1;
+    console.log("Bedtime!");
+    sleepinessLevel.textContent = Charmander.sleepiness;
+  }
 }
 sleepButton.addEventListener("click", sleepPet);
 
 // PLAY BUTTON DECREASES BOREDNESS BY 1 WHEN CLICKED
 
-//EXERCSE BUTTON DECREASES BOREDNESS BY 1 AND NCREASES TIREDNESS BY 1 WHEN CLICKED
+let playButton = document.getElementById("play");
+function playPet() {
+  if (Charmander.boredom >= 0) {
+    Charmander.boredom = Charmander.boredom - 1;
+    alert("Let's play!");
+    console.log("Let's play!");
+    boredomLevel.textContent = Charmander.boredom;
+  } else {
+    Charmander.boredom === 0;
+  }
+}
+playButton.addEventListener("click", playPet);
 
-// RESET GAME BUTTON
+//EXERCSE BUTTON DECREASES BOREDNESS BY 1 AND NCREASES HUNGER BY 1 WHEN CLICKED
+
+let exerciseButton = document.getElementById("exercise");
+function exercisePet() {
+  Charmander.boredom = Charmander.boredom - 1;
+  Charmander.hunger = Charmander.hunger + 1;
+  alert("That was fun, but I'm hungry!");
+  console.log("That was fun, but I'm hungry!");
+  boredomLevel.textContent = Charmander.boredom;
+  hungerLevel.textContent = Charmander.hunger;
+}
+
+exerciseButton.addEventListener("click", exercisePet);
+
+// RESET GAME BUTTON - RESETS TO ORIGINAL LOADED PAGE
 
 $("#reset").click(function () {
   location.reload(true);
   alert("Let's try again!");
 });
+
+// NAME YOUR PET
 
 var subButton = document.getElementById("subButton");
 subButton.addEventListener("click", getUserName, false);
@@ -191,19 +177,17 @@ subButton.addEventListener("click", getUserName, false);
 //Submit name button dissappears when clicked
 
 $("#subButton").on("click", (event) => {
-  // $(event.currentTarget) refers to the '#menu' element that was clicked.
   $(event.currentTarget).hide();
   timer();
 });
 
-$("#sleep").on("click", function () {
+//SCREEN SWITCHES TO A NIGHT SCENE WHEN ZZZZ BUTTON IS CLICKED
+
+$("#sleep").on("click", () => {
   $("container").toggleClass("dark");
-  // $("#sleep").html("☀️Wake up!☀️");
 });
 
-//Would love to make the ipiut field hide when subButton is clicked. Work on that.
-
-//Click on 'Adopt Charmander' button to hatch egg to Charmander
+//CLCIK ON ADOPT CHARMANDER TO CHANGE EGG TO CHARMANDER
 
 $("#charmander").on("click", () => {
   $("#egg").attr(
@@ -212,7 +196,7 @@ $("#charmander").on("click", () => {
   );
 });
 
-// Click Charmeleon button to evolve when prompted.
+// CLICK CHARMELEON BUTTON WHEN PROMPTED TO EVOLVE TO CHARMELEON
 
 $("#charmeleon").on("click", () => {
   $("#egg").attr(
@@ -222,7 +206,7 @@ $("#charmeleon").on("click", () => {
   return false;
 });
 
-//Click Charlizard button to evolve when prompted
+// CLICK CHARLIZARD BUTTON WHEN PROMPTED TO EVOLVE TO CHARLIZARD
 
 $("#charlizard").on("click", () => {
   $("#egg").attr(
@@ -232,59 +216,51 @@ $("#charlizard").on("click", () => {
   return false;
 });
 
-// REFERENCE FOR BUTTONS:
-
-//     <div class="meters" id="hunger">Hunger Level: <span id="hunger-level"></span></div>
-//     <div class="meters" id="sleepiness">Tiredness Level: <span id="sleepiness-level"></span></div>
-//     <div class="meters" id="boredom">Boredom Level: <span id="boredom-level"></span></div>
-//     <div class="meters" id="age">Age: <span id="age-level"></span></div>
-
-//use jquery .fadeToggle to fade in and out a toy when button is clicked
-
-$("#play").click(function () {
-  Charmander.boredom = Charmander.boredom - 1;
-});
-
-// Potentially use sligeToggle to make the moon rise and fall when sleep is clicked. Also, change opacity so the screen darkens when sleep is clicked. Do the opposite for wake up (or take wake up out if I can set a timer of sorts on this function.
-// $('#sleep').on('click', () => {
-//     $('#sleep').slideToggle();
-// // });
-
-// EXAMPLE 2:
-// $(document).ready(function () {
-//   // Set background image of a div on click of the button
-
-// Trying to change background when sleep button is clicked
-
-// $("#sleep").click(function () {
-//   $(".container").toggleClass(".container-night");
-// });
-
-// $("#sleep").click(function () {
-//   $("backgounde").toggle("slow", 0.5, function () {});
-// });
-
-// $("#sleep").click(function () {
-//   $(".container").("slow", 100);
-// });
-
-// $(".sleep").on({
-//   click: function () {
-
-///progress meters - this works and the bar slowly gets bigger - need to figure out how to make smaller when buttons are clicked
-//https://www.w3schools.com/howto/howto_js_progressbar.asp
-
 // END OF GAME
-const endOfGame = function () {
+// let endOfGame = function () {
+//   if (
+//     hungerLevel >= 10 ||
+//     sleepinessLevel >= 10 ||
+//     boredomLevel >= 10 ||
+//     ageLevel >= 10
+//   ) {
+//     alert("Game over!");
+//   }
+// };
+
+// endOfGame();
+
+// if (
+//   Charmander.hunger >= 10 ||
+//   Charmander.sleepiness >= 10 ||
+//   Charmander.boredom >= 10 ||
+//   Charmander.age >= 10
+// ) {
+//   console.log("Game over!");
+// }
+
+let endOfGame = function () {
   if (
-    Charmander.hunger >= 10 ||
-    Charmander.sleepiness >= 10 ||
-    Charmander.boredom >= 10 ||
-    Charmander.age >= 10 ||
-    timer.val === 0
+    Charmander.hunger > 10 ||
+    Charmander.sleepiness > 10 ||
+    Charmander.boredom > 10 ||
+    Charmander.age > 10
   ) {
-    alert("Game over!");
+    clearInterval(timer);
+    alert("Your pet needed more attention! Please try again.");
   }
 };
 
 endOfGame();
+
+//Working on health alerts when numbers reach a certain point, but cna't figure it out.
+
+// let healthAlert = function () {
+//   if (
+//     (Charmander.age >= 1 && Charmander.hunger >= 7) ||
+//     (Charmander.age >= 1 && Charmander.sleepiness >= 7) ||
+//     (Charmander.age >= 1 && Charmander.boredom >= 7)
+//   );
+//   alert("Your pet needs some attention!");
+//   healthAlert();
+// };
